b990e9f9ce6f316d2c0a68e88edd80b910806cf7
mikaeldjawa <mikaeldjawa18@gmail.com>
2025-12-01 21:45:52 +0700

    initial commit

--- src/lib/axios-instance.ts
+++ src/lib/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dummyjson.com/user",
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
 });
 
 const getAccessToken = () => localStorage.getItem("accessToken");
